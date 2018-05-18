e964a549b6d7d0755dbecad7383d8da8f7bb625d
Assem Chelli <assem.ch@gmail.com>
2018-05-18 04:45:09 +0100

    use flags

--- bot/logic.py
+++ bot/logic.py
@@ -4,6 +4,7 @@ import alfanous
 
 def answer(query):
     response = alfanous.do(flags={"action":"search", "query":query, "unit": "aya", "highlight": None, "limit":1})
+    print response["error"]
     if (response["search"]["interval"]["total"]):
          reply = "{" + response["search"]["ayas"][1]["identifier"]["sura_arabic_name"] + " "+ str(response["search"]["ayas"][1]["identifier"]["aya_id"]) + "}";
          reply += "\n{"+ response["search"]["ayas"][1]["aya"]["text"]+"}"
