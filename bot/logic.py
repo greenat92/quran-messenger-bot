8bca6e811f21e6c51365fb7472655bada0478322
Assem Chelli <assem.ch@gmail.com>
2018-05-18 04:33:10 +0100

    search exact

--- bot/logic.py
+++ bot/logic.py
@@ -3,33 +3,15 @@
 import alfanous
 
 def answer(token):
-    return alfanous.search(token)["search"]["interval"]["total"]
+    response = alfanous.search(token)
+    if (response["search"]["interval"]["total"]):
+         reply = "{" + response["search"]["ayas"][1]["identifier"]["sura_arabic_name"] + " "+ response["search"]["ayas"][1]["identifier"]["aya_id"] + "}";
+         reply += "\n{"+ response["search"]["ayas"][1]["aya"]["text"]+"}"
+    else:
+        reply = None
+    return reply
 
 LOGIC_RESPONSES = {
-    'account': [
-        "You can find your account information on https://www.codingforentrepreneurs.com/account/"
-    ],
-    'enroll': [
-        "You can simply enroll on https://www.codingforentrepreneurs.com/enroll/"
-    ],
-    "projects": [
-        "All of our projects are on http://joincfe.com/projects and courses are on http://joincfe.com/courses/"
-    ],
-    "free": [
-        "All of our free content is on http://joincfe.com/youtube/."
-    ],
-    "membership": [
-        "You can enroll for a pro membership on http://joincfe.com/enroll/ to get access to all of our content in HD!"
-    ],
-    "afford": [
-         "Have you thought about subscribing to our free YouTube channel?  It's on http://joincfe.com/youtube/."
-    ],
-    "blog": [
-        "Our blog is on http://joincfe.com/blog/"
-    ],
-    "guides": [
-        "You can access our guides blog is on http://joincfe.com/blog/"
-    ],
     "thank": [
          "Of course!",
         "Anytime!",
@@ -50,7 +32,4 @@ LOGIC_RESPONSES = {
         "Have you considered looking at our code on http://joincfe.com/github/? That might help you",
         "We don't review code at this time, but you can consider looking at our open-source repo http://joincfe.com/github/"
     ],
-    'human': [
-        'A real person will get back to you shortly!'
-    ]
 }
