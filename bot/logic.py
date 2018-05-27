b08ea361e6f2611b5ab5863f68c840c693fbecde
Assem Chelli <assem.ch@gmail.com>
2018-05-27 20:52:56 +0100

    increase limit to 5

--- bot/logic.py
+++ bot/logic.py
@@ -3,12 +3,13 @@
 import alfanous
 
 def answer(query):
-    response = alfanous.do(flags={"action":"search", "query":query, "unit": "aya", "highlight": "none", "limit":3})
+    limit = 5
+    response = alfanous.do(flags={"action":"search", "query":query, "unit": "aya", "highlight": "none", "limit": limit})
     print query.encode('utf-8')
     print response["error"]
     reply=""
     if (not response["error"]["code"]):
-        for i in xrange(1,  min(4, response["search"]["interval"]["total"]+1)):
+        for i in xrange(1,  min(limit+1, response["search"]["interval"]["total"]+1)):
              reply += "{" + response["search"]["ayas"][i]["identifier"]["sura_arabic_name"] + " "+ str(response["search"]["ayas"][i]["identifier"]["aya_id"]) + "}"
              reply += "\n"+ response["search"]["ayas"][i]["aya"]["text"]
              reply += "\n\n"
