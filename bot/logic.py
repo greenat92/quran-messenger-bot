c0c28e5c85bcfd823aa570276aff2864d9699dba
Assem Chelli <assem.ch@gmail.com>
2018-05-27 20:46:28 +0100

    fix min

--- bot/logic.py
+++ bot/logic.py
@@ -7,7 +7,7 @@ def answer(query):
     print query.encode('utf-8')
     print response["error"]
     if (not response["error"]["code"]):
-        for i in xrange(1,  response["search"]["interval"]["total"]+1):
+        for i in xrange(1,  min(4, response["search"]["interval"]["total"]+1)):
              reply = "{" + response["search"]["ayas"][i]["identifier"]["sura_arabic_name"] + " "+ str(response["search"]["ayas"][i]["identifier"]["aya_id"]) + "}"
              reply += "\n"+ response["search"]["ayas"][i]["aya"]["text"]
              reply += "\n-----\n"
